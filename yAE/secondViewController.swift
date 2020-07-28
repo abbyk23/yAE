108f7a2ac91e306fc56f27918db7af85b1aa77fb
Abby Krier <abbykrier@gmail.com>
2020-07-28 11:27:47 -0500

    First step in final project

---
+++ yAE/secondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewController.swift
+//  yAE
+//
+//  Created by Abby Krier on 7/28/20.
+//  Copyright © 2020 Abby Krier. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
